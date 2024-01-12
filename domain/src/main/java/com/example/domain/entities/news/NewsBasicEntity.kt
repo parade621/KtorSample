01f35bae5d5e514be849e15fd03f0693594cbd55
몰름보반장 <parade621@naver.com>
2024-01-12 22:09:38 +0900

    엔티티 수정 중

---
+++ domain/src/main/java/com/example/domain/entities/news/NewsBasicEntity.kt
@@ -0,0 +1,12 @@
+package com.example.domain.entities.news
+
+data class NewsBasicEntity(
+    val publisher: String = "",
+    val author: String = "",
+    val title: String = "",
+    val description: String = "",
+    val url: String = "",
+    val urlToImage: String = "",
+    val publishedAt: String = "",
+    val content: String = ""
+)
