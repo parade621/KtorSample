1f455877a900b884e21419facd63427a818d6d95
몰름보반장 <parade621@naver.com>
2024-01-18 23:12:17 +0900

    network 적용

--- core/data/src/main/java/com/example/data/repository/NewsSearchRepoImpl.kt
+++ core/data/src/main/java/com/example/data/repository/NewsSearchRepoImpl.kt
@@ -1,9 +1,28 @@
 package com.example.data.repository
 
+import com.example.data.model.Article
 import com.example.data.source.remote.NewsSearchDataSource
+import com.example.domain.model.ApiResult
+import com.example.domain.repository.NewsSearchRopo
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-//class NewsSearchRepoImpl @Inject constructor(
-//    private val newsSearchDataSource: NewsSearchDataSource
-//) : NewsSearchRepo{
-//}
+class NewsSearchRepoImpl @Inject constructor(
+    private val newsSearchDataSource: NewsSearchDataSource
+) : NewsSearchRopo {
+    override fun getNewsDataResponse(
+        q: String,
+        searchIn: String,
+        sources: String,
+        domains: String,
+        excludeDomains: String,
+        from: String,
+        to: String,
+        language: String,
+        sortBy: String,
+        pageSize: Int,
+        page: Int
+    ): Flow<ApiResult<Article>> {
+        TODO("Not yet implemented")
+    }
+}
